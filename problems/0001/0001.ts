2af679e8188fca3b2f538ff56c374d303f4afe62
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-12-02 20:18:41 +0800

    well begun is half done

--- problems/0001/0001.ts
+++ problems/0001/0001.ts
@@ -1,25 +1,23 @@
-import { AbstractSolution,RunSolution } from "../../utils/solution";
+import { AbstractSolution, RunSolution } from "../../utils/solution";
 @RunSolution
-export class Solution1 extends AbstractSolution{
+export class Solution1 extends AbstractSolution {
   getProblemName(): string {
-      return 'Multiples of 3 or 5'
+    return "Multiples of 3 or 5";
   }
   solve() {
-      return this.doSolve(1000)
+    return this.doSolve(19564);
   }
-  private getSumOfDivisors(num:number,limit:number):number{
-     let sum=0
-     for(let i=num;i<limit;i+=num)
-     {
-      sum+=i
-     }
-     return sum
+  private getSumOfDivisors(num: number, limit: number): number {
+    const up = Math.floor(limit / num);
+    const sum1toN = ((up + 1) * up) / 2;
+    return sum1toN * num;
   }
-  private doSolve(limit:number){
-      let totalSum=0
-      totalSum+=this.getSumOfDivisors(3,limit)
-      totalSum+=this.getSumOfDivisors(5,limit)
-      totalSum-=this.getSumOfDivisors(15,limit)
-      return totalSum
+
+  private doSolve(limit: number) {
+    let totalSum = 0;
+    totalSum += this.getSumOfDivisors(3, limit);
+    totalSum += this.getSumOfDivisors(5, limit);
+    totalSum -= this.getSumOfDivisors(15, limit);
+    return totalSum;
   }
 }
